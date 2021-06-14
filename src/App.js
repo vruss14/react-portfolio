b8f738c055d8b007ebba5dc148c0ea5382e76259
Valerie Russell <vruss14@gmail.com>
2021-06-14 17:14:09 -0400

    refactored projectsthrough mapping and using a JSON file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import ContentContainer from "./components/ContentContainer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import About from "./components/About";
 import Project from "./components/Project";
+import projects from "./utils/Projects";
 import Footer from "./components/Footer"
 import Small from "./components/Small"
- 
-function App() {
-  return (
-    <div>
-      <ContentContainer>
-        <Header />
-        <Main>
-          <About />
-          <Project />
-        </Main>
-        <Footer />
-      </ContentContainer>
-      <Small />
+import ProjectContainer from "./components/ProjectContainer";
+
+class App extends Component {
+  state = {
+    projects
+  };
+
+  render() {
+    return (
+      <div>
+        <ContentContainer>
+          <Header />
+          <Main>
+            <About />
+
+            <ProjectContainer>
+            
+                {this.state.projects.map(project => (
+                
+                <Project
+                  id={project.id}
+                  key={project.id}
+                  image={project.image}
+                  alt={project.alt}
+                  title={project.title}
+                  description={project.description}
+                  deployed_url={project.deployed_url}
+                  repo_url={project.repo_url}
+                />
+              ))}
+
+          </ProjectContainer>
+
+          </Main>
+          <Footer />
+        </ContentContainer>
+        <Small />
     </div>
 
-
-  );
+    );
+  }
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div>
+//       <ContentContainer>
+//         <Header />
+//         <Main>
+//           <About />
+//           <Project 
+//             />
+//         </Main>
+//         <Footer />
+//       </ContentContainer>
+//       <Small />
+//     </div>
+
+
+//   );
+// }
+
+// export default App;
---
+++ src/components/ProjectContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProjectContainer(props) {
+    return (
+        <section id="completed-projects">{props.children}</section>
+    )
+}
+
+export default ProjectContainer;
