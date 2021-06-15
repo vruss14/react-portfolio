fcb0870eba7d179d9667bd468278fc68f229ace5
Valerie Russell <vruss14@gmail.com>
2021-06-15 16:23:02 -0400

    modified paths for HashRouter

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
         <div>
           <Switch>
 
-          <Route exact path= "/#/">
+          <Route exact path= "/">
               <ContentContainer>
               <Header />
               <Main>
@@ -60,7 +60,7 @@ class App extends Component {
               <Small />
             </Route>
 
-            <Route path="/#projects">
+            <Route exact path="/#projects">
               <ContentContainer>
               <Header />
               <Main>
